76af533183b67a6d0f72329c265fa16e851f8503
Michal Murin <mmurin.dev@gmail.com>
2019-02-13 16:25:21 +0100

    Flights activity

---
+++ app/src/main/java/com/murin/dailyflights/FlightsActivity.kt
@@ -0,0 +1,35 @@
+package com.murin.dailyflights
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.murin.dailyflights.databinding.ActivityFlightsBinding
+
+class FlightsActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityFlightsBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_flights
+        )
+
+        navController = Navigation.findNavController(this, R.id.flights_nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, null)
+
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
