e33e202796ccc0e44ac3bfd5009b472d112eed2b
Michal Murin <mmurin.dev@gmail.com>
2019-02-14 21:52:30 +0100

    FLight detail: show the same as in an item, show kiwi opener

--- app/src/main/java/com/murin/dailyflights/ui/detail/FlightDetailFragment.kt
+++ app/src/main/java/com/murin/dailyflights/ui/detail/FlightDetailFragment.kt
@@ -1,13 +1,17 @@
 package com.murin.dailyflights.ui.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.murin.dailyflights.databinding.FragmentFlightDetailBinding
 
 class FlightDetailFragment : Fragment() {
+    private val kiwiPackageName = "com.skypicker.main"
     private lateinit var binding: FragmentFlightDetailBinding
 
     override fun onCreateView(
@@ -17,7 +21,24 @@ class FlightDetailFragment : Fragment() {
     ): View? {
         binding = FragmentFlightDetailBinding.inflate(inflater, container, false)
 
-        binding.flight = arguments?.getParcelable("flight")
+
+
+        with(binding) {
+            flight = arguments?.getParcelable("flight")
+
+            activity?.packageManager?.let { pm ->
+                if (pm.getInstalledPackages(0).any { it.packageName == kiwiPackageName }) {
+
+                    ivShowInKiwi.setImageDrawable(pm.getApplicationIcon(kiwiPackageName))
+                    llShowInKiwi.visibility = VISIBLE
+                    llShowInKiwi.setOnClickListener {
+                        val kiwiIntent = Intent(Intent.ACTION_VIEW, Uri.parse(binding.flight?.deepLink ?: ""))
+                        startActivity(kiwiIntent)
+                    }
+                }
+            }
+
+        }
 
         setHasOptionsMenu(true)
         return binding.root
